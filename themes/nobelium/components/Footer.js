9140c453217e23292923f81beec5d7684f14b4df
look23-beep <dclook23@gmail.com>
2026-02-28 05:25:30 +0800

    Update Footer.js

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -1,15 +1,1 @@
-import { siteConfig } from '@/lib/config'
 
-export const Footer = (props) => {
-  const { post } = props
-  const fullWidth = post?.fullWidth ?? false
-
-  return (
-    <footer
-      className={`z-10 relative mt-6 flex-shrink-0 m-auto w-full transition-all ${
-        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
-      }`}
-    >
-    </footer>
-  )
-}

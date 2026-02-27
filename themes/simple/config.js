a913cff2417fd0ef529ec34c89a9f2c3ebd086fb
look23-beep <dclook23@gmail.com>
2026-02-28 03:58:35 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
   SIMPLE_LOGO_IMG: '/Logo.webp',
-  SIMPLE_TOP_BAR: true, // 显示顶栏
+  SIMPLE_TOP_BAR: false, // 显示顶栏
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
   SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '',
 
@@ -14,9 +14,9 @@ const CONFIG = {
   SIMPLE_ARTICLE_RECOMMEND_POSTS: process.env.NEXT_PUBLIC_SIMPLE_ARTICLE_RECOMMEND_POSTS || false, // 文章详情底部显示推荐
 
   // 菜单配置
-  SIMPLE_MENU_CATEGORY: true, // 显示分类
-  SIMPLE_MENU_TAG: true, // 显示标签
-  SIMPLE_MENU_ARCHIVE: true, // 显示归档
-  SIMPLE_MENU_SEARCH: true // 显示搜索
+  SIMPLE_MENU_CATEGORY: false, // 显示分类
+  SIMPLE_MENU_TAG: false, // 显示标签
+  SIMPLE_MENU_ARCHIVE: false, // 显示归档
+  SIMPLE_MENU_SEARCH: false // 显示搜索
 }
 export default CONFIG
